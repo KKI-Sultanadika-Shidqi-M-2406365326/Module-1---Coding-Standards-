ae2768a8d13e6e5b89f3a8e0a928d9b8b80e70d2
Twentism <melantongabriel@gmail.com>
2026-03-10 20:12:47 +0700

    refactor: fix empty list bug in OrderController and optimize OrderRepository

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,6 +16,13 @@ class OrderControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    // TAMBAHKAN BARIS INI:
+    @MockBean
+    private PaymentService paymentService;
 
     @Test
     void testCreateOrderPage() throws Exception {
