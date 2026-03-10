1d323beacefd9e1704dffb0548365ff1033e5a48
sultanadika <sultanmumtazsami@gmail.com>
2026-03-10 19:29:36 +0700

    [Green] Addedfunctional test for ordercontroller and paymentcontroller

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/PaymentControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/PaymentControllerTest.java
@@ -1,18 +1,29 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PaymentController.class)
-public class PaymentControllerTest {
+class PaymentControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    PaymentService paymentService;
 
     @Test
     void testPaymentDetailPage() throws Exception {
@@ -20,5 +31,54 @@ public class PaymentControllerTest {
         mockMvc.perform(get("/payment/detail"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("payment-detail"));
+    }
+
+    @Test
+    void testPaymentDetailById() throws Exception {
+
+        Payment payment = new Payment("1", null, "VOUCHER", null);
+
+        when(paymentService.getPayment("1")).thenReturn(payment);
+
+        mockMvc.perform(get("/payment/detail/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payment-detail-view"));
+    }
+
+    @Test
+    void testPaymentAdminList() throws Exception {
+
+        List<Payment> payments = new ArrayList<>();
+
+        when(paymentService.getAllPayments()).thenReturn(payments);
+
+        mockMvc.perform(get("/payment/admin/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payment-list"));
+    }
+
+    @Test
+    void testPaymentAdminDetail() throws Exception {
+
+        Payment payment = new Payment("1", null, "BANK_TRANSFER", null);
+
+        when(paymentService.getPayment("1")).thenReturn(payment);
+
+        mockMvc.perform(get("/payment/admin/detail/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payment-admin-detail"));
+    }
+
+    @Test
+    void testSetPaymentStatus() throws Exception {
+
+        Payment payment = new Payment("1", null, "BANK_TRANSFER", null);
+
+        when(paymentService.getPayment("1")).thenReturn(payment);
+
+        mockMvc.perform(post("/payment/admin/set-status/1")
+                        .param("status", "SUCCESS"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payment-admin-detail"));
     }
 }
